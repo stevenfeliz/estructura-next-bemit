2250d3dfbc7ad913df0e58137e3f649d0d387fc8
sdeleon <steven.deleonfeliz@gmail.com>
2023-06-23 20:11:01 -0400

    estructura next js bemit primer commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'normalize.css/normalize.css';
+import '../bemit/bemit_main.scss';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
